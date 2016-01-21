68dd5348460c31f176c8933fada59ab246013e9d
Watcharamet Chitsanukup Srinaethirothai <platoosom@gmail.com>
2016-01-21 17:51:11 +0700

    Test jenkins

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -20,7 +20,7 @@ class ExampleTest extends TestCase
     public function testBasicFail()
     {
         $this->visit('/')
-            ->see('Laravel 1');
+            ->see('Laravel 2');
     }
 
 }

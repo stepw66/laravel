574489295169da857ac84f70238e5fe25ad299a4
Select2Web <platoosom@gmail.com>
2017-02-01 17:00:21 +0700

    เอากลับมาให้แล้วไอ้น้อง

---
+++ database/migrations/2015_08_20_050842_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->string('title', 255);
+            $table->string('content');
+            $table->timestamps();
+            $table->timestamp('published_at')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
